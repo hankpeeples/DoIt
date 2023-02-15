d5033868d08c7d2c0137d07d2e706597cd729902
Henry Peeples <hgpeeples48@gmail.com>
2023-02-14 23:24:10 -0600

    add todo card component props

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,17 +9,21 @@ function App() {
         <div className={styles.halfContainer}>
           <h3 className={styles.halfHeading}>All Tasks</h3>
           <div className={styles.listContainer}>
-            <TodoCard text="Left card" />
-            <TodoCard text="Left card with more content to see what happens" />
-            <TodoCard text="Left card" />
-            <TodoCard text="Left card" />
+            <TodoCard text='Left card' priority='prioThree' today={false} />
+            <TodoCard
+              text='Left card with more content to see what happens and now I need more text'
+              priority='prioOne'
+              today={false}
+            />
+            <TodoCard text='Left card' priority='prioTwo' today={false} />
+            <TodoCard text='Left card' priority='prioOne' today={false} />
           </div>
         </div>
         <div className={styles.halfContainer}>
           <h3 className={styles.halfHeading}>Today</h3>
           <div className={styles.listContainer}>
-            <TodoCard text="Right card" />
-            <TodoCard text="Right card" />
+            <TodoCard text='Right card' priority='prioOne' today={true} />
+            <TodoCard text='Right card' priority='prioTwo' today={true} />
           </div>
         </div>
       </div>
