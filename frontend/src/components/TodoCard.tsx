e7e16ad3b0bbe3bb3030bc0c5e8c3ea88eb949ba
Henry Peeples <hgpeeples48@gmail.com>
2023-02-14 22:49:09 -0600

    add icons and set layout

--- frontend/src/components/TodoCard.tsx
+++ frontend/src/components/TodoCard.tsx
@@ -1,11 +1,33 @@
+import { useState } from 'react';
 import styles from './TodoCard.module.css';
+import {
+  ArrowRightCircleIcon,
+  TrashIcon,
+  PencilSquareIcon,
+  DocumentDuplicateIcon,
+} from '@heroicons/react/24/solid';
 
 type Props = {
   text: string,
+  priority?: string,
 };
 
-const TodoCard = ({ text }: Props) => {
-  return <div className={styles.cardWrapper}>{text}</div>;
+const TodoCard = ({ text, priority }: Props) => {
+  return (
+    <div className={styles.cardWrapper}>
+      <div className={styles.row}>
+        <p>{text}</p>
+      </div>
+      <div className={styles.iconContainer}>
+        <div className={styles.iconContainerLeft}>
+          <TrashIcon className={styles.icon} />
+          <PencilSquareIcon className={styles.icon} />
+          <DocumentDuplicateIcon className={styles.icon} />
+        </div>
+        <ArrowRightCircleIcon className={styles.icon} onClick={() => {}} />
+      </div>
+    </div>
+  );
 };
 
 export default TodoCard;
