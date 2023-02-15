6416d917bea19a0e3f1cd7e398e95d06e701beb2
Henry Peeples <hgpeeples48@gmail.com>
2023-02-14 23:23:27 -0600

    update to top priority color bar

--- frontend/src/components/TodoCard.tsx
+++ frontend/src/components/TodoCard.tsx
@@ -1,30 +1,46 @@
-import { useState } from 'react';
 import styles from './TodoCard.module.css';
 import {
-  ArrowRightCircleIcon,
+  ArrowRightIcon,
+  ArrowLeftIcon,
   TrashIcon,
   PencilSquareIcon,
   DocumentDuplicateIcon,
-} from '@heroicons/react/24/solid';
+  CheckIcon,
+} from '@heroicons/react/24/outline';
 
 type Props = {
   text: string,
-  priority?: string,
+  priority: string,
+  today: boolean,
 };
 
-const TodoCard = ({ text, priority }: Props) => {
+const TodoCard = ({ text, priority, today }: Props) => {
   return (
     <div className={styles.cardWrapper}>
-      <div className={styles.row}>
-        <p>{text}</p>
-      </div>
+      <div className={`${priority} ${styles.prioTopBar}`} />
+      <p>{text}</p>
       <div className={styles.iconContainer}>
-        <div className={styles.iconContainerLeft}>
-          <TrashIcon className={styles.icon} />
-          <PencilSquareIcon className={styles.icon} />
-          <DocumentDuplicateIcon className={styles.icon} />
-        </div>
-        <ArrowRightCircleIcon className={styles.icon} onClick={() => {}} />
+        {today ? (
+          <>
+            <ArrowLeftIcon className={styles.icon} onClick={() => {}} />
+            <div className={styles.iconContainerLeft}>
+              <CheckIcon className={styles.icon} />
+              <DocumentDuplicateIcon className={styles.icon} />
+              <PencilSquareIcon className={styles.icon} />
+              <TrashIcon className={styles.icon} />
+            </div>
+          </>
+        ) : (
+          <>
+            <div className={styles.iconContainerLeft}>
+              <TrashIcon className={styles.icon} />
+              <PencilSquareIcon className={styles.icon} />
+              <DocumentDuplicateIcon className={styles.icon} />
+              <CheckIcon className={styles.icon} />
+            </div>
+            <ArrowRightIcon className={styles.icon} onClick={() => {}} />
+          </>
+        )}
       </div>
     </div>
   );
