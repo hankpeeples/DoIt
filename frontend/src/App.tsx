c2b940880ff9b43837ba861da08353acac658a08
Henry Peeples <hgpeeples48@gmail.com>
2023-02-18 00:58:21 -0600

    update todo card props

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,21 +9,22 @@ function App() {
         <div className={styles.halfContainer}>
           <h3 className={styles.halfHeading}>All Tasks</h3>
           <div className={styles.listContainer}>
-            <TodoCard text='Left card' priority='prioThree' today={false} />
+            <TodoCard text='Left card' priority='prioThree' today={false} dueDate='Jan 01' />
             <TodoCard
               text='Left card with more content to see what happens and now I need more text'
               priority='prioOne'
               today={false}
+              dueDate='Jan 01'
             />
-            <TodoCard text='Left card' priority='prioTwo' today={false} />
-            <TodoCard text='Left card' priority='prioOne' today={false} />
+            <TodoCard text='Left card' priority='prioTwo' today={false} dueDate='' />
+            <TodoCard text='Left card' priority='prioOne' today={false} dueDate='Jan 01' />
           </div>
         </div>
         <div className={styles.halfContainer}>
           <h3 className={styles.halfHeading}>Today</h3>
           <div className={styles.listContainer}>
-            <TodoCard text='Right card' priority='prioOne' today={true} />
-            <TodoCard text='Right card' priority='prioTwo' today={true} />
+            <TodoCard text='Right card' priority='prioOne' today={true} dueDate='Jan 01' />
+            <TodoCard text='Right card' priority='prioTwo' today={true} dueDate='Jan 01' />
           </div>
         </div>
       </div>
