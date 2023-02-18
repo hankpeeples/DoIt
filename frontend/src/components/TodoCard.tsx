eb82cfda604d100f1542e1c081871f5881814a32
Henry Peeples <hgpeeples48@gmail.com>
2023-02-18 00:57:55 -0600

    remove bottom icon bar

--- frontend/src/components/TodoCard.tsx
+++ frontend/src/components/TodoCard.tsx
@@ -1,46 +1,18 @@
 import styles from './TodoCard.module.css';
-import {
-  ArrowRightIcon,
-  ArrowLeftIcon,
-  TrashIcon,
-  PencilSquareIcon,
-  DocumentDuplicateIcon,
-  CheckIcon,
-} from '@heroicons/react/24/outline';
+import DueBar from './DueBar';
 
 type Props = {
   text: string,
   priority: string,
   today: boolean,
+  dueDate: string,
 };
 
-const TodoCard = ({ text, priority, today }: Props) => {
+const TodoCard = ({ text, priority, today, dueDate }: Props) => {
   return (
     <div className={`${styles.cardWrapper} ${priority}`}>
+      <DueBar dueDate={dueDate} />
       <p>{text}</p>
-      <div className={styles.iconContainer}>
-        {today ? (
-          <>
-            <ArrowLeftIcon className={styles.icon} onClick={() => {}} />
-            <div className={styles.iconContainerLeft}>
-              <CheckIcon className={styles.icon} />
-              <DocumentDuplicateIcon className={styles.icon} />
-              <PencilSquareIcon className={styles.icon} />
-              <TrashIcon className={styles.icon} />
-            </div>
-          </>
-        ) : (
-          <>
-            <div className={styles.iconContainerLeft}>
-              <TrashIcon className={styles.icon} />
-              <PencilSquareIcon className={styles.icon} />
-              <DocumentDuplicateIcon className={styles.icon} />
-              <CheckIcon className={styles.icon} />
-            </div>
-            <ArrowRightIcon className={styles.icon} onClick={() => {}} />
-          </>
-        )}
-      </div>
     </div>
   );
 };
